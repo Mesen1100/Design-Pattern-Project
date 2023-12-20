4aa8a3e8163c9a5bfb65f75f65255d73d71b037b
Mustafa Esen <mustafaesen1100@gmail.com>
2023-12-20 20:29:37 +0300

    Create Furniture for observer

---
+++ src/ObserverPatternMembers/Furniture.java
@@ -0,0 +1,27 @@
+package ObserverPatternMembers;
+
+import java.util.Date;
+
+import ObserverPattern.Observer;
+
+public class Furniture implements Observer {
+    private String name;
+    private Date date;
+
+    public Furniture(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Process is complete for" + name);
+    }
+    @Override
+    public void update() {
+        Date newdate = new Date(System.currentTimeMillis());
+        if(newdate.after(this.date)) {
+            display();
+        }
+    }
+}
