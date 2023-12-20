1c9fd93c8ed901e12bbc8c107beb245654813386
Mustafa Esen <mustafaesen1100@gmail.com>
2023-12-21 02:10:24 +0300

    Add Command Pattern Test Code

--- src/App.java
+++ src/App.java
@@ -1,24 +1,46 @@
 import java.util.Date;
 
+import CommandPatternMembers.Commands.AirConditionerHigh;
+import CommandPatternMembers.Commands.AirConditionerLow;
+import CommandPatternMembers.Commands.AirConditionerOff;
+import CommandPatternMembers.Commands.AirConditionerOn;
+import CommandPatternMembers.Furnitures.AirConditioner;
+import CommandPatternMembers.Furnitures.RemoteControl;
 import ObserverPatternMembers.Furniture;
 import ObserverPatternMembers.Person;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*  // Observer Pattern
-        Person person = new Person("John");
+        /*  Observer Pattern Test
+         *  
+         * Person person = new Person("John");
+         * 
+         * Furniture chair = new Furniture("Chair", new Date(System.currentTimeMillis()
+         * + 10000));
+         * Furniture table = new Furniture("Table", new Date(System.currentTimeMillis()
+         * + 20000));
+         * 
+         * person.registerObserver(chair);
+         * person.registerObserver(table);
+         * 
+         * while (true) {
+         * person.notifyObservers();
+         * Thread.sleep(1000);
+         * }
+         */
 
-        Furniture chair = new Furniture("Chair", new Date(System.currentTimeMillis() + 10000));
-        Furniture table = new Furniture("Table", new Date(System.currentTimeMillis() + 20000));
-
-        person.registerObserver(chair);
-        person.registerObserver(table);
-
-        while (true) {
-            person.notifyObservers();
-            Thread.sleep(1000);
-        }
-        */
-    
+        /*  Command Pattern Test
+         * 
+         * AirConditioner air = new AirConditioner();
+         * AirConditionerOff off = new AirConditionerOff(air);
+         * AirConditionerOn on = new AirConditionerOn(air);
+         * AirConditionerHigh high = new AirConditionerHigh(air);
+         * AirConditionerLow low = new AirConditionerLow(air);
+         * RemoteControl control = new RemoteControl();
+         * control.setCommand(0, on, off);
+         * control.setCommand(1, high, low);
+         * control.toString();
+         * 
+         */
     }
 }
