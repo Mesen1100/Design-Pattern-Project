d8623ce6040f219e0e88d4d5c11ff8211108c06f
Mustafa Esen <mustafaesen1100@gmail.com>
2023-12-21 15:33:57 +0300

    Create Test Code for Music Player

--- src/App.java
+++ src/App.java
@@ -4,15 +4,21 @@ import CommandPatternMembers.Commands.AirConditionerHigh;
 import CommandPatternMembers.Commands.AirConditionerLow;
 import CommandPatternMembers.Commands.AirConditionerOff;
 import CommandPatternMembers.Commands.AirConditionerOn;
+import CommandPatternMembers.Commands.MusicPlayerDown;
+import CommandPatternMembers.Commands.MusicPlayerOff;
+import CommandPatternMembers.Commands.MusicPlayerOn;
+import CommandPatternMembers.Commands.MusicPlayerUp;
 import CommandPatternMembers.Furnitures.AirConditioner;
+import CommandPatternMembers.Furnitures.MusicPlayer;
 import CommandPatternMembers.Furnitures.RemoteControl;
 import ObserverPatternMembers.Furniture;
 import ObserverPatternMembers.Person;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*  Observer Pattern Test
-         *  
+        /*
+         * Observer Pattern Test
+         * 
          * Person person = new Person("John");
          * 
          * Furniture chair = new Furniture("Chair", new Date(System.currentTimeMillis()
@@ -29,18 +35,40 @@ public class App {
          * }
          */
 
-        /*  Command Pattern Test
+        /*
+         * Command Pattern Test
          * 
+         * 
+         * RemoteControl control=new RemoteControl();
+         * 
+         * Create Air Conditioner and Command
          * AirConditioner air = new AirConditioner();
-         * AirConditionerOff off = new AirConditionerOff(air);
-         * AirConditionerOn on = new AirConditionerOn(air);
-         * AirConditionerHigh high = new AirConditionerHigh(air);
-         * AirConditionerLow low = new AirConditionerLow(air);
-         * RemoteControl control = new RemoteControl();
-         * control.setCommand(0, on, off);
-         * control.setCommand(1, high, low);
-         * control.toString();
+         * AirConditionerOff airoff = new AirConditionerOff(air);
+         * AirConditionerOn airon = new AirConditionerOn(air);
+         * AirConditionerHigh airhigh = new AirConditionerHigh(air);
+         * AirConditionerLow airlow = new AirConditionerLow(air);
+         * 
+         * Set Command to Remote Controller
+         * control.setCommand(0, airon, airoff);
+         * control.setCommand(1, airhigh, airlow);
+         * 
+         * 
+         * Create Air Conditioner and Command
+         * MusicPlayer music=new MusicPlayer();
+         * MusicPlayerOn musicon=new MusicPlayerOn(music);
+         * MusicPlayerOff musicoff =new MusicPlayerOff(music);
+         * MusicPlayerDown musicdown=new MusicPlayerDown(music);
+         * MusicPlayerUp musicup=new MusicPlayerUp(music);
+         * 
+         * Set Command to Remote Controller
+         * control.setCommand(2, musicon, musicoff);
+         * control.setCommand(3, musicup, musicdown);
+         * 
+         * 
+         * System.out.println(control.toString());
          * 
          */
+
+        
     }
 }
