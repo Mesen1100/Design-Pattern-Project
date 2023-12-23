dc783c2564601abef87445641b4845448971154a
Mustafa Esen <mustafaesen1100@gmail.com>
2023-12-23 13:00:02 +0300

    Update TV with TV Behaviors

--- src/CommandPatternMembers/Furnitures/TV.java
+++ src/CommandPatternMembers/Furnitures/TV.java
@@ -1,38 +1,60 @@
 package CommandPatternMembers.Furnitures;
 
+import StrategyPattern.TVBehavior;
+import StrategyPatternMembers.TVNormalBehavior;
+
 public class TV {
     private boolean active;
     private int channel;
+    private TVBehavior behavior;
 
-    public TV(){
-
+    public TV() {
+        behavior = new TVNormalBehavior();
     }
 
-    public void on(){
-        active=true;
+    public TV(TVBehavior tvBehavior) {
+        this.behavior = tvBehavior;
+    }
+
+    public void on() {
+        active = true;
         System.out.println("TV is Open");
         printChannel();
     }
 
-    public void off(){
-        active=false;
+    public void off() {
+        active = false;
         System.out.println("Tv is Closed");
     }
 
-    public void up(){
-        if(channel<200){
-            channel++;
+    public void up() {
+        if (active) {
+            channel=behavior.up(channel);
+            printChannel();
+        } else {
+            System.out.println("TV is not open");
         }
-        printChannel();
-    }
-    public void down(){
-        if(channel>0){
-            channel--;
-        }
-        printChannel();
+
     }
 
-    public void printChannel(){
-        System.out.println("Channel is "+ channel);
+    public void down() {
+        if (active) {
+            channel=behavior.down(channel);
+            printChannel();
+        } else {
+            System.out.println("TV is not open");
+        }
+
+    }
+
+    public void printChannel() {
+        System.out.println("Channel is " + channel);
+    }
+
+    public void setBehavior(TVBehavior behavior) {
+        this.behavior = behavior;
+    }
+    public void setChannel(int channel) {
+        this.channel = channel;
     }
 }
