91f6ad301fe2b3d457021919d9ac151db428904c
Mustafa Esen <mustafaesen1100@gmail.com>
2023-12-23 12:59:43 +0300

    TV Behavior for TV

---
+++ src/StrategyPatternMembers/TVSportBehavior.java
@@ -0,0 +1,46 @@
+package StrategyPatternMembers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import StrategyPattern.TVBehavior;
+
+public class TVSportBehavior implements TVBehavior{
+    ArrayList<Integer> sportchannel;
+    int sayi;
+    
+    public TVSportBehavior() throws Exception{
+        sportchannel=new ArrayList<>();
+        File file=new File("src/Database/sportchannel.txt");
+        Scanner scanner=new Scanner(file);
+        while(scanner.hasNext()){
+            sportchannel.add(scanner.nextInt());
+        }
+        scanner.close();
+        sayi=0;
+    }
+    @Override
+    public int down(int channel) {
+        if(sayi==0){
+            sayi=sportchannel.size()-1;
+        }
+        else{
+            sayi--;
+        }
+
+        return sportchannel.get(sayi);
+    }
+
+    @Override
+    public int up(int channel) {
+        if(sayi==sportchannel.size()-1){
+            sayi=0;
+        }
+        else{
+            sayi++;
+        }
+        return sportchannel.get(sayi);
+    }
+}
